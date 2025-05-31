c7677a5e853418067e8f0265b6f51bf1d3d255f0
omniiamne <89515958152@mail.ru>
2025-05-31 20:43:56 +0700

    Update config.js

--- styles/config.js
+++ styles/config.js
@@ -2,7 +2,7 @@
 /* Sheet ID
 /* Your sheet ID
 /* ------------------------------------------------------------------- */
-let sheetID = "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak";
+let sheetID = "1efLyB-lO8ytEnXDF1FhJ8R6V2CTu5ElQV-zC8rI18EE";
 
 
 /* ------------------------------------------------------------------- */
